9dda1d056d67391cc7f40a00db7672b13728c8e1
sivaraj003 <sivarajsiva003@gmail.com>
2022-05-31 21:55:25 +0530

    tamil

---
+++ src/org/tt/Sample1.java
@@ -0,0 +1,11 @@
+package org.tt;
+
+
+public class Sample1{
+	public static void main(String[] args) {
+		System.out.println("tamil");
+		System.out.println("done");
+		System.out.println("siva");
+	}
+
+}
